020b956e4d96a489a314527992994ccbbf526749
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2020-04-12 20:19:59 +0300

    Trim fields in db

--- src/db.js
+++ src/db.js
@@ -1,15 +1,19 @@
+function trimAllFields(obj) {
+  return Object.entries(obj).reduce((acc, [k, v]) => ({ ...acc, [k.trim()]: v.trim() }), {});
+}
+
 export default new Promise((resolve) => {
   // eslint-disable-next-line no-undef
   Tabletop.init({
     key: 'https://docs.google.com/spreadsheets/d/1kmC3_ayBMAS5xks7ba11GwXdYfS7Bm4LEWFif_yeLD4/edit?usp=sharing',
     callback: (data) => resolve({
       pages: data.pages.elements.map((page) => ({
-        ...page,
+        ...trimAllFields(page),
         images: data.images.elements
           .filter((image) => image.page === page.page)
-          .map((i) => ({ ...i, description: i.description.trim() })),
+          .map((i) => trimAllFields(i)),
       })),
-      contact: data.contact.elements,
+      contact: data.contact.elements.map((i) => trimAllFields(i)),
     }),
     simpleSheet: false,
   });
